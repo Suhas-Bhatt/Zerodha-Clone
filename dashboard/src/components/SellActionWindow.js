3f2a809f1663ad41b68b2ec23efe7c3ac8a7d211
suhas-bhatt <suhasbhat1204@gmail.com>
2026-01-30 19:32:01 +0530

    Initial commit

---
+++ dashboard/src/components/SellActionWindow.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import GeneralContext from "./GeneralContext";
+import "./SellActionWindow.css";
+
+const SellActionWindow = ({ uid }) => {
+  const [stockQuantity, setStockQuantity] = useState(1);
+  const [stockPrice, setStockPrice] = useState(0.0);
+  const { closeSellWindow } = useContext(GeneralContext);
+
+  const handleSellClick = async () => {
+    try {
+      await axios.post("http://localhost:3002/newOrder", {
+        name: uid,
+        qty: stockQuantity,
+        price: stockPrice,
+        mode: "SELL",
+      });
+      closeSellWindow();
+    } catch (error) {
+      console.error("Failed to place sell order:", error);
+    }
+  };
+
+  const handleCancelClick = () => {
+    closeSellWindow();
+  };
+
+  const estimatedProceeds = (stockQuantity * stockPrice).toFixed(2);
+
+  return (
+    <div className="container" id="sell-window" draggable="true">
+      <div className="regular-order">
+        <div className="inputs">
+          <fieldset>
+            <legend>Qty.</legend>
+            <input
+              type="number"
+              name="qty"
+              id="qty"
+              value={stockQuantity}
+              onChange={(e) => setStockQuantity(Number(e.target.value))}
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>Price</legend>
+            <input
+              type="number"
+              name="price"
+              id="price"
+              step="0.05"
+              value={stockPrice}
+              onChange={(e) => setStockPrice(Number(e.target.value))}
+            />
+          </fieldset>
+        </div>
+      </div>
+
+      <div className="buttons">
+        <span>Estimated proceeds ₹{estimatedProceeds}</span>
+        <div>
+          <button className="btn btn-red" onClick={handleSellClick}>
+            Sell
+          </button>
+          <button className="btn btn-grey" onClick={handleCancelClick}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SellActionWindow;
